e1000e53d0315966f92550374727164380baae0f
Tran Nhut <tnhut803@gmail.com>
2022-06-10 21:34:26 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,17 @@ const connectdb = async () => {
 };
 connectdb();
 route(app);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 app.listen(process.env.PORT, () => {
   console.log(`App listening at ${process.env.PORT}`);
 });
