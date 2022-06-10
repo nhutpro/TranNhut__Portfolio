88af1f1825cecdaa751659445084745f02765a8a
Tran Nhut <tnhut803@gmail.com>
2022-06-10 21:28:54 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(morgan("combined"));
 
 require("dotenv").config();
 app.use(cors());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 const connectdb = async () => {
